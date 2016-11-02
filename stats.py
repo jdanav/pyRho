e890ebc3636b4e4b3954811d00bf53513e53b488
jdanav <jdanav@gmail.com>
2016-11-02 11:02:59 +0000

    Add files via upload

--- stats.py
+++ stats.py
@@ -3,39 +3,54 @@ from tree import Tree
 
 def Rho(tree):
 
+    if 'Rho' in tree.tree.values()[0].extra.keys():
+        print 'Rho already computed'
+        return tree.tree.values()[0].extra['Rho']
     mutCount = 0
     for leaf in tree.leaves:
         mutCount += mutationCount(leaf, tree)
     rho = float(mutCount)/ len(tree.leaves)
+    tree.tree.values()[0].extra['Rho'] = \
+        (rho, len(tree.leaves), '{:.3f}'.format(rho))
     return (rho, len(tree.leaves), '{:.3f}'.format(rho))
 
 
 def StDev(tree):
 
+    if 'SE' in tree.tree.values()[0].extra.keys():
+        return tree.tree.values()[0].extra['SE']
     tSum = 0.0
     for node in tree.tree.values()[1:]:
         if node.name in tree.leaves: tSum += len(node.mutations)
         else: tSum += len(node.mutations) * \
                       (len(Tree('',tree.subtree(node.name)).leaves)) **2
     sd = sqrt(tSum/(len(tree.leaves)**2))
+    tree.tree.values()[0].extra['SE'] = (sd, '{:.3f}'.format(sd))
     return (sd, '{:.3f}'.format(sd))
 
 
 def Age(tree):
 
+    if 'Age' in tree.tree.values()[0].extra.keys():
+        return tree.tree.values()[0].extra['Age']
     rho = Rho(tree)[0]
     age = (exp(-exp(-0.0263 *(rho + 40.2789))) *rho *3624)
+    tree.tree.values()[0].extra['Age'] = (age, '{:.3f}'.format(age))
     return (age, '{:.3f}'.format(age))
 
 
 def ConfidenceInterval(tree):
-    
+
+    if 'CI' in tree.tree.values()[0].extra.keys():
+        return tree.tree.values()[0].extra['CI']
     rho = Rho(tree)[0]
     sd = StDev(tree)[0]
     lower = max(exp(-exp(-0.0263 * ((rho - (1.96 * sd)) + 40.2789)))* \
             (rho - (1.96 * sd)) * 3624.0, 0)
     upper = exp(-exp(-0.0263 * ((rho + (1.96 * sd)) + 40.2789)))* \
             (rho + (1.96 * sd)) * 3624.0;
+    tree.tree.values()[0].extra['CI'] = \
+        '{:.3f}'.format(lower), '{:.3f}'.format(upper)
     return '{:.3f}'.format(lower), '{:.3f}'.format(upper)
 
 
