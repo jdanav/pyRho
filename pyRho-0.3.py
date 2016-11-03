fdbe7b7b0591c5ea0ef3abc0affce1a7f68d46b6
jdanav <jdanav@gmail.com>
2016-11-03 11:15:14 +0000

    Add files via upload

--- pyRho-0.3.py
+++ pyRho-0.3.py
@@ -7,7 +7,7 @@ import ttk, tkFileDialog
 
 
 root = Tk()
-root.title("PyRHO 0.3")
+root.title("PyRHO 0.33")
 
 filename = tkFileDialog.askopenfilename(parent = root)
 types = tkFileDialog.askopenfilename(parent = root)
@@ -22,10 +22,7 @@ f2S = Frame(nb)
 
 for frame in [main, f1S, f2S]:
 
-    frame.tree = ttk.Treeview(frame, height = 20, columns = \
-                ('Mutations', 'Type', 'Leaves', u'ρ (Rho)', 'SE', \
-                 'Age', 'Confidence interval', 'Leaves f1', 'f1', \
-                 'SE f1', 'Leaves f2', 'f2', 'SE f2'))
+    frame.tree = ttk.Treeview(frame, height = 20)
     frame.tree.column("#0",minwidth = 350, width = 400)
     frame.scroll = ttk.Scrollbar(frame, orient=VERTICAL, \
                                  command=frame.tree.yview)
@@ -35,55 +32,84 @@ for frame in [main, f1S, f2S]:
     frame.label = ttk.Label(frame, text= \
                             '%s nodes and %s leaves in %s layers' % \
                            (len(n.nodes), len(n.leaves), len(n.layers)))
-
-    for column in frame.tree['columns']:
-        frame.tree.heading(column, text=column, anchor= 'w')
-        frame.tree.column(column, width = 50, stretch = True)
-    frame.tree.column('Age', width = 60)
-    frame.tree.column('Mutations', width = 65)
-    frame.tree.column('Confidence interval', width = 150)
-    frame.tree.column('Type', width = 65)
+    
     frame.tree.insert('',0,'None', open = True)
-    frame.tree.heading("#0",text="File path:\t%s" % n.name, anchor = 'w') 
-
-
-    for node in n.tree.values():
-        if node.name in n.leaves:
-            frame.tree.insert(node.parent,'end', \
-                             iid = node.name, text = node.name, \
-                        values = (len(node.mutations), \
-                                  node.isSource(),'--','--','--','--','--',\
-                                  '--','--','--','--','--','--'))
-        elif node.name in n.nodes:
-            f1 = fN(n, node.name, 1)
-            f2 = fN(n, node.name, 2)
-            xnode = Tree(node.name,n.subtree(node.name))
-            frame.tree.insert(str(node.parent),'end', iid = node.name, \
-                             text = node.name, values = (len(node.mutations), \
-                                (node.isSource() if node.children == [] else node.type),\
-                                Rho(xnode)[1], Rho(xnode)[2], \
-                                StDev(xnode)[1], Age(xnode)[1], \
-                                ConfidenceInterval(xnode), f1[0][1],\
-                                f1[0][2], f1[1][1], f2[0][1], \
-                                f2[0][2], f2[1][1]), open = True)
+    frame.tree.heading("#0",text="File path:\t%s" % n.name, anchor = 'w')
 
     frame.label.grid(row = 1, sticky=(W))
     frame.scroll.grid(row = 0, column = 1, sticky=(N,S))
     frame.tree.grid(row = 0, column = 0)
 
-main.tree["displaycolumns"] = ('Mutations','Leaves',u'ρ (Rho)', 'SE', 'Age', 'Confidence interval')
 
-f1S.tree["displaycolumns"] = ('Mutations','Type','Leaves f1','f1','SE f1')
-f1S.tree.heading('Leaves f1', text='Leaves', anchor= 'w')
-f1S.tree.heading('SE f1', text='SE', anchor= 'w')
 
-f2S.tree["displaycolumns"] = ('Mutations','Type','Leaves f2','f2','SE f2')
-f2S.tree.heading('Leaves f2', text='Leaves', anchor= 'w')
-f2S.tree.heading('SE f2', text='SE', anchor= 'w')
+    
+main.tree['columns'] = ('Mutations','Leaves', u'ρ (Rho)', 'SE', 'Age', \
+                        'Confidence interval')
+for column in main.tree['columns']:
+    main.tree.heading(column, text=column, anchor= 'w')
+    main.tree.column(column, width = 50, stretch = True)
+main.tree.column('Age', width = 60)
+main.tree.column('Mutations', width = 65)
+main.tree.column('Confidence interval', width = 150)
+
+for node in n.tree.values():
+    if node.name in n.leaves:
+        main.tree.insert(node.parent,'end', \
+                         iid = node.name, text = node.name, \
+                    values = (len(node.mutations), '--','--','--','--','--'))
+    elif node.name in n.nodes:
+        xnode = Tree(node.name,n.subtree(node.name))
+        main.tree.insert(str(node.parent),'end', iid = node.name, \
+                         text = node.name, values = (len(node.mutations), \
+                                            Rho(xnode)[1], Rho(xnode)[2], \
+                                            StDev(xnode)[1], Age(xnode)[1], \
+                                            ConfidenceInterval(xnode)), open = True)
+
+
+    
+f1S.tree['columns'] = ('Mutations','Type','Leaves','f1','SE')
+for column in f1S.tree['columns']:
+    f1S.tree.heading(column, text=column, anchor= 'w')
+    f1S.tree.column(column, width = 50, stretch = True)
+f1S.tree.column('Type', width = 65)
+f1S.tree.column('Mutations', width = 65)
+
+for node in n.tree.values():
+    if node.name in n.leaves:
+        f1S.tree.insert(node.parent,'end', \
+                         iid = node.name, text = node.name, \
+                    values = (len(node.mutations), node.isSource(),'--','--','--'))
+    elif node.name in n.nodes:
+        f1 = fN(n, node.name, 1)
+        xnode = Tree(node.name,n.subtree(node.name))
+        f1S.tree.insert(str(node.parent),'end', iid = node.name, \
+                         text = node.name, values = (len(node.mutations), node.isSource(), \
+                                                     f1[0][1], f1[0][2], f1[1][1]), open = True)
+
+
+
+f2S.tree['columns'] = ('Mutations','Type','Leaves','f2','SE')
+for column in f2S.tree['columns']:
+    f2S.tree.heading(column, text=column, anchor= 'w')
+    f2S.tree.column(column, width = 50, stretch = True)
+f2S.tree.column('Type', width = 65)
+f2S.tree.column('Mutations', width = 65)
+
+for node in n.tree.values():
+    if node.name in n.leaves:
+        f2S.tree.insert(node.parent,'end', \
+                         iid = node.name, text = node.name, \
+                    values = (len(node.mutations), node.isSource(),'--','--','--'))
+    elif node.name in n.nodes:
+        f2 = fN(n, node.name, 2)
+        xnode = Tree(node.name,n.subtree(node.name))
+        f2S.tree.insert(str(node.parent),'end', iid = node.name, \
+                         text = node.name, values = (len(node.mutations), node.isSource(), \
+                                                     f2[0][1], f2[0][2], f2[1][1]), open = True)
+
 
 nb.add(main, text = "Tree information")
 nb.add(f1S, text = "f1 statistics")
 nb.add(f2S, text = "f2 statistics")
 
 root.mainloop()
-
