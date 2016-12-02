f44f5c835b1499888d3b7cf445c019d1c07240ba
jdanav <jdanav@gmail.com>
2016-12-02 01:28:23 +0000

    Add files via upload

--- pyRho-0.66.py
+++ pyRho-0.66.py
@@ -8,12 +8,15 @@ import ttk, tkFileDialog
 
 root = Tk()
 root.title("0.66")
+root.iconbitmap(default = 'favicon.ico')
+root.label = ttk.Label(root, text = '%s nodes and %s leaves in %s layers' % (0,0,0))
+root.label.pack(side = BOTTOM, anchor = 'w')
 
 menubar = Menu(root)
 
 filemenu = Menu(menubar, tearoff = 0)
 exportmenu = Menu(menubar, tearoff = 0)
-viewmenu = Menu(menubar, tearoff = 0)
+optmenu = Menu(menubar, tearoff = 0)
 nb = ttk.Notebook(root)
 nb.enable_traversal()
 nb.pack(fill = BOTH, expand = 1)
@@ -41,7 +44,6 @@ def toggleLeaves():
         for frame in [main, f1S, f2S]:
             t = frame.tree; t.detach(*n.leaves)
         visible = 0
-
     else:
         for frame in [main, f1S, f2S]:
             t = frame.tree; t.detach(*n.leaves)
@@ -52,23 +54,17 @@ def toggleLeaves():
 
 
 for frame in [main, f1S, f2S]:
-
     frame.tree = ttk.Treeview(frame, height = 20);
     frame.tree.column("#0",minwidth = 350, width = 400)
     frame.scroll = ttk.Scrollbar(frame, orient = VERTICAL, command = frame.tree.yview)
     frame.tree.bind('<Control-c>', getItem)
     frame.tree.configure(yscrollcommand = frame.scroll.set)
     frame.tree['selectmode'] = 'extended'
-    frame.label = ttk.Label(frame, text = '%s nodes and %s leaves in %s layers' % (0,0,0))
     frame.tree.insert('',0,'None', open = True)
     frame.tree.heading("#0",text = "File path:\t%s" % '', anchor = 'w')
-
-    frame.label.pack(side = BOTTOM, anchor = 'w')
     frame.scroll.pack(side = RIGHT, fill = BOTH)
     frame.tree.pack(fill = BOTH, expand = 1)
-
     frame.tree.tag_configure('leaf', background = '#ffffe0')
-    frame.tree.tag_configure('node', background = '#ffffff')
      
 main.tree['columns'] = ('Mutations','Leaves', u'ρ (Rho)', 'SE', 'Age', 'Confidence interval')
 for column in main.tree['columns']:
@@ -79,19 +75,14 @@ main.tree.column('Mutations', width = 65)
 main.tree.column('Confidence interval', width = 150)
 
 f1S.tree['columns'] = ('Mutations','Type','Leaves','f1','SE')
-for column in f1S.tree['columns']:
-    f1S.tree.heading(column, text = column, anchor = 'w')
-    f1S.tree.column(column, width = 50, stretch = True)
-f1S.tree.column('Type', width = 65)
-f1S.tree.column('Mutations', width = 65)
-
 f2S.tree['columns'] = ('Mutations','Type','Leaves','f2','SE', 'f2+')
-for column in f2S.tree['columns']:
-    f2S.tree.heading(column, text = column, anchor = 'w')
-    f2S.tree.column(column, width = 50, stretch = True)
-f2S.tree.column('Type', width = 65)
-f2S.tree.column('Mutations', width = 65)
-
+for frame in [f1S,f2S]:
+    for column in frame.tree['columns']:
+        frame.tree.heading(column, text = column, anchor = 'w')
+        frame.tree.column(column, width = 50, stretch = True)
+    frame.tree.column('Type', width = 65)
+    frame.tree.column('Mutations', width = 65)
+    
 nb.add(main, text = "Tree information")
 nb.add(f1S, text = "f1 statistics")
 nb.add(f2S, text = "f2 statistics")
@@ -101,15 +92,15 @@ def openXML():
 
     try:
         global n, visible, filemenu, menubar
-        filename = tkFileDialog.askopenfilename(parent = root)
+        filename = tkFileDialog.askopenfilename(parent = root, filetypes = [('XML files', '.xml'), ('all files', '.*')])
         n = Tree(str(filename))
 
         for frame in [main, f1S, f2S]:
-            frame.label['text'] = '%s nodes and %s leaves in %s layers' % (len(n.nodes), len(n.leaves), len(n.layers))
             frame.tree.heading("#0",text = "File path:\t%s" % filename, anchor = 'w')
             frame.tree.delete('None')
             frame.tree.insert('',0,'None', open = True)
             visible = 1            
+        root.label['text'] = '%s nodes and %s leaves in %s layers' % (len(n.nodes), len(n.leaves), len(n.layers))
 
         i = 0
         for node in n.tree.values():
@@ -129,8 +120,9 @@ def openXML():
             sys.stdout.write('\rPopulating tree... %.2f%%' % (float(i)/len(n.tree)*100))
         print '\n'
         filemenu.entryconfig(1, state = "normal")
-        viewmenu.entryconfig(1, state = "normal")
-        menubar.entryconfig(3, state = "normal")
+        filemenu.entryconfig(3, state = "normal")
+        filemenu.entryconfig(4, state = "normal")
+        menubar.entryconfig(2, state = "normal")
         main.tree.focus_set()
         
     except: pass
@@ -140,50 +132,91 @@ def openTypes():
 
     try:
         global n
-        types = tkFileDialog.askopenfilename(parent = root)
+        types = tkFileDialog.askopenfilename(parent = root, filetypes = [('text files', '.txt'), ('all files', '.*')])
         n.updateTypes(str(types))
         i = 0.0
-        for node in n.nodes:
+        for node in n.tree:
             node = n.tree[node]
             f1 = n.fN(node.name, 1)
             f2 = n.fN(node.name, 2)
             f1S.tree.item(node.name, values = (len(node.mutations), node.isSource(), f1[0], f1[1], f1[2]))
             f2S.tree.item(node.name, values = (len(node.mutations), node.isSource(), f2[0], f2[1], f2[2], n.f2plus(node.name)))
             i += 1
-            sys.stdout.write('\rUpdating tree... %.2f%%' % ((i/(len(n.nodes)))*100))
+            sys.stdout.write('\rUpdating tree... %.2f%%' % ((i/(len(n.tree)))*100))
         print '\n'
     except: pass
 
         
 def exportNewick():
 
-    save = tkFileDialog.asksaveasfilename(parent = root)
+    global n
+    save = tkFileDialog.asksaveasfilename(parent = root, initialfile = "%s_newick.txt" % n.root.name)
     try: f = open(save, 'w'); f.write(n.Newick(None)); f.close()
     except: pass
 
-def exportCurrent(): pass
+def saveTable():
 
+    global n
+    current = root.nametowidget(root.focus_get())
+    save = tkFileDialog.asksaveasfilename(parent = root, initialfile = "%s_table.txt" % n.root.name)
+    try:
+        f = open(save,'w')
+        header = ['Node','Leaves','Rho','Standard error','Age','Confidence interval','f1 leaves','f1 Rho','f1 SE','f2 leaves','f2 Rho','f2 SE','f2+ eligible']
+        if current == main.tree:
+            header = header[:6]
+            f.write('\t'.join(header))
+            for node in n.nodes:
+                w = [node] + [str(i) for i in main.tree.item(node)['values'][1:]]
+                f.write('\n' + '\t'.join(w))
+        elif current == f1S.tree:
+            header = header[0] + header[6:9]
+            f.write('\t'.join(header))
+            for node in n.nodes:
+                w = [node] + [str(i) for i in f1S.tree.item(node)['values'][2:]]
+                f.write('\n' + '\t'.join(w))
+        else:
+            header = header[0] + header[9:]
+            f.write('\t'.join(header))
+            for node in n.nodes:
+                w = [node] + [str(i) for i in f2S.tree.item(node)['values'][2:]]
+                f.write('\n' + '\t'.join(w))
+        f.close()
+        sys.stdout.write('%s successfully created\n' % (save))
+    except: pass
+    
+            
+def saveAll():
 
-def exportAll(): pass
+    global n
+    save = tkFileDialog.asksaveasfilename(parent = root, initialfile = "%s_table.txt" % n.root.name)
+    try:
+        f = open(save,'w')
+        f.write('\t'.join(['Node','Leaves','Rho','Standard error','Age','Confidence interval','f1 leaves','f1 Rho','f1 SE','f2 leaves','f2 Rho','f2 SE','f2+ eligible']))
+        for node in n.nodes:
+            w = [node] + [str(i) for i in main.tree.item(node)['values'][1:]] + [str(i) for i in f1S.tree.item(node)['values'][2:]] + [str(i) for i in f2S.tree.item(node)['values'][2:]]
+            f.write('\n' + '\t'.join(w))
+        f.close()
+        sys.stdout.write('%s successfully created\n' % (save))
+    except: pass
     
 
-exportmenu.add_command(label = "Current table (CSV)", command = exportCurrent)
-exportmenu.add_command(label = "Full table (CSV)", command = exportAll)
-exportmenu.add_command(label = "Tree (Newick format)", command = exportNewick)
-
-viewmenu.insert_command(1, label = "Show/Hide leaves", command = toggleLeaves)
-viewmenu.entryconfig(1, state = "disabled")
+optmenu.add_command(label = "Export tree (Newick format)", command = exportNewick)
+optmenu.add_command(label = "Show/Hide leaves", command = toggleLeaves)
 
 filemenu.add_command(label = "Open XML file", command = openXML)
-filemenu.insert_command(1, label = "Import node types", command = openTypes)
+filemenu.add_command(label = "Import node types", command = openTypes)
 filemenu.entryconfig(1, state = "disabled")
+filemenu.add_separator()
+filemenu.add_command(label = "Save current table (TSV)", command = saveTable) 
+filemenu.add_command(label = "Save all (TSV)", command = saveAll) 
+filemenu.entryconfig(3, state = "disabled")
+filemenu.entryconfig(4, state = "disabled")
 filemenu.add_separator()
 filemenu.add_command(label = "Quit", command = root.destroy)
 
 menubar.add_cascade(label = "File", menu = filemenu)
-menubar.add_cascade(label = "View", menu = viewmenu)
-menubar.add_cascade(label = "Export", menu = exportmenu)
-menubar.entryconfig(3, state = "disabled")
+menubar.add_cascade(label = "Options", menu = optmenu)
+menubar.entryconfig(2, state = "disabled")
 
 root.config(menu = menubar)
 root.mainloop()
