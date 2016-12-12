bdf4accf8d7727279759dd9d612183ccae0899dc
jdanav <jdanav@gmail.com>
2016-12-12 14:51:58 +0000

    Add files via upload

---
+++ pyRho-0.7.py
@@ -0,0 +1,268 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+from Tkinter import *
+from tree import *
+import ttk, tkFileDialog, tkSimpleDialog
+
+
+root = Tk()
+root.title("0.7")
+root.iconbitmap(default = 'favicon.ico')
+root.label = ttk.Label(root, text = '--')
+root.label.pack(side = BOTTOM, anchor = 'w')
+
+menubar = Menu(root)
+
+filemenu = Menu(menubar, tearoff = 0)
+exportmenu = Menu(menubar, tearoff = 0)
+optmenu = Menu(menubar, tearoff = 0)
+nb = ttk.Notebook(root)
+nb.enable_traversal()
+nb.pack(fill = BOTH, expand = 1)
+
+main = Frame(nb)
+f1S = Frame(nb)
+f2S = Frame(nb)
+
+visible = 1
+
+n = None
+
+def getItem(a):
+    
+    w = root.nametowidget(root.focus_get())
+    root.clipboard_clear()
+    root.clipboard_append(w.item(w.focus()))
+    print root.selection_get(selection = "CLIPBOARD")
+
+
+def toggleLeaves():
+    
+    global visible
+    if visible: 
+        for frame in [main, f1S, f2S]:
+            t = frame.tree; t.detach(*n.leaves)
+        visible = 0
+    else:
+        for frame in [main, f1S, f2S]:
+            t = frame.tree; t.detach(*n.leaves)
+            for item in n.leaves:
+                parent = n.tree[n.tree[item].parent]
+                t.reattach(item,parent.name,parent.children.index(item))
+        visible = 1
+
+
+for frame in [main, f1S, f2S]:
+    frame.tree = ttk.Treeview(frame, height = 20);
+    frame.tree.column("#0",minwidth = 350, width = 400)
+    frame.scroll = ttk.Scrollbar(frame, orient = VERTICAL, command = frame.tree.yview)
+    frame.tree.bind('<Control-c>', getItem)
+    frame.tree.configure(yscrollcommand = frame.scroll.set)
+    frame.tree['selectmode'] = 'extended'
+    frame.tree.insert('',0,'None', open = True)
+    frame.tree.heading("#0",text = "File path:\t%s" % '', anchor = 'w')
+    frame.scroll.pack(side = RIGHT, fill = BOTH)
+    frame.tree.pack(fill = BOTH, expand = 1)
+    frame.tree.tag_configure('leaf', background = '#ffffe0')
+     
+main.tree['columns'] = ('Mutations','Leaves', u'ρ (Rho)', 'SE', 'Age', 'Confidence interval')
+for column in main.tree['columns']:
+    main.tree.heading(column, text=column, anchor = 'w')
+    main.tree.column(column, width = 50, stretch = True)
+main.tree.column('Age', width = 60)
+main.tree.column('Mutations', width = 65)
+main.tree.column('Confidence interval', width = 150)
+
+f1S.tree['columns'] = ('Mutations','Type','Leaves','f1','SE')
+f2S.tree['columns'] = ('Mutations','Type','Leaves','f2','SE', 'f2+')
+for frame in [f1S,f2S]:
+    for column in frame.tree['columns']:
+        frame.tree.heading(column, text = column, anchor = 'w')
+        frame.tree.column(column, width = 50, stretch = True)
+    frame.tree.column('Type', width = 65)
+    frame.tree.column('Mutations', width = 65)
+    
+nb.add(main, text = "Tree information")
+nb.add(f1S, text = "f1 statistics")
+nb.add(f2S, text = "f2 statistics")
+
+        
+def openXML():
+
+    try:
+        global n, visible, filemenu, menubar
+        filename = tkFileDialog.askopenfilename(parent = root, filetypes = [('XML files', '.xml'), ('all files', '.*')])
+        n = Tree(str(filename))
+
+        for frame in [main, f1S, f2S]:
+            frame.tree.heading("#0",text = "File path:\t%s" % filename, anchor = 'w')
+            frame.tree.delete('None'); frame.tree.insert('',0,'None', open = True)
+            visible = 1            
+        root.label['text'] = '%s nodes and %s leaves in %s layers' % (len(n.nodes), len(n.leaves), len(n.layers))
+
+        i = 0
+        for node in n.tree.values():
+
+            if node.name in n.leaves:
+                main.tree.insert(node.parent,'end', iid = node.name, text = node.name, tags = ('leaf'), values = (len(node.mutations), '--','--','--','--','--'))
+                f1S.tree.insert(node.parent,'end', iid = node.name, text = node.name, tags = ('leaf'), values = (len(node.mutations), node.isSource(),'--','--','--'))
+                f2S.tree.insert(node.parent,'end', iid = node.name, text = node.name, tags = ('leaf'), values = (len(node.mutations), node.isSource(),'--','--','--', '--'))
+            elif node.name in n.nodes:
+                xnode = n.subtrees[node.name]
+                f1 = n.fN(node.name, 1)
+                f2 = n.fN(node.name, 2)
+                main.tree.insert(str(node.parent),'end', iid = node.name, text = node.name, tags = ('node'), values = (len(node.mutations), len(set(xnode.keys()) & set (n.leaves)), n.Rho(node.name, xnode), n.StErr(xnode), n.Age(node.name), n.ConfidenceInterval(node.name)), open = True)
+                f1S.tree.insert(str(node.parent),'end', iid = node.name, text = node.name, tags = ('node'), values = (len(node.mutations), '--', f1[0], f1[1], f1[2]), open = True)
+                f2S.tree.insert(str(node.parent),'end', iid = node.name, text = node.name, tags = ('node'), values = (len(node.mutations), '--', f2[0], f2[1], f2[2], n.f2plus(node.name)), open = True)
+            i += 1
+            sys.stdout.write('\rPopulating tree... %s/%s' % (i, len(n.tree)))
+        sys.stdout.write('\n')
+        for i in [1,4,5]: filemenu.entryconfig(i, state = "normal")
+        menubar.entryconfig(2, state = "normal")
+        main.tree.focus_set()
+    except: sys.stdout.write('\n')
+    
+
+def openTypes():
+
+    try:
+        global n
+        types = tkFileDialog.askopenfilename(parent = root, filetypes = [('text files', '.txt'), ('all files', '.*')])
+        n.updateTypes(str(types))
+        i = 0
+        for node in n.tree:
+            node = n.tree[node]
+            f1 = n.fN(node.name, 1)
+            f2 = n.fN(node.name, 2)
+            f1S.tree.item(node.name, values = (len(node.mutations), node.isSource(), f1[0], f1[1], f1[2]))
+            f2S.tree.item(node.name, values = (len(node.mutations), node.isSource(), f2[0], f2[1], f2[2], n.f2plus(node.name)))
+            i += 1
+            sys.stdout.write('\rUpdating tree... %s/%s' % (i, len(n.tree)))
+        sys.stdout.write('\n')
+        root.label['text'] = '%s nodes and %s leaves in %s layers (%s sources and %s sinks, %s undefined)' % (len(n.nodes), len(n.leaves), len(n.layers), n.nsrc, n.nsnk, n.nudf)
+    except: sys.stdout.write('\n')
+
+        
+def exportNewick():
+
+    global n
+    save = tkFileDialog.asksaveasfilename(parent = root, initialfile = "%s_newick.txt" % n.root.name)
+    try: f = open(save, 'w'); f.write(n.Newick(None)); f.close()
+    except: pass
+
+
+def saveTable():
+
+    global n
+    current = root.nametowidget(root.focus_get())
+    save = tkFileDialog.asksaveasfilename(parent = root, initialfile = "%s_table.txt" % n.root.name)
+    try:
+        f = open(save,'w')
+        header = ['Node','Leaves','Rho','Standard error','Age','Confidence interval','f1 leaves','f1 Rho','f1 SE','f2 leaves','f2 Rho','f2 SE','f2+ eligible']
+        if current == main.tree:
+            header = header[:6]
+            f.write('\t'.join(header))
+            for node in n.nodes:
+                w = [node] + [str(i) for i in main.tree.item(node)['values'][1:]]
+                f.write('\n' + '\t'.join(w))
+        elif current == f1S.tree:
+            header = [header[0]] + header[6:9]
+            f.write('\t'.join(header))
+            for node in n.nodes:
+                w = [node] + [str(i) for i in f1S.tree.item(node)['values'][2:]]
+                f.write('\n' + '\t'.join(w))
+        else:
+            header = [header[0]] + header[9:]
+            f.write('\t'.join(header))
+            for node in n.nodes:
+                w = [node] + [str(i) for i in f2S.tree.item(node)['values'][2:]]
+                f.write('\n' + '\t'.join(w))
+        f.close()
+        sys.stdout.write('%s successfully created\n' % (save))
+    except: sys.stdout.write('\n')
+    
+            
+def saveAll():
+
+    global n
+    save = tkFileDialog.asksaveasfilename(parent = root, initialfile = "%s_table.txt" % n.root.name)
+    try:
+        f = open(save,'w')
+        f.write('\t'.join(['Node','Leaves','Rho','Standard error','Age','Confidence interval','f1 leaves','f1 Rho','f1 SE','f2 leaves','f2 Rho','f2 SE','f2+ eligible']))
+        for node in n.nodes:
+            w = [node] + [str(i) for i in main.tree.item(node)['values'][1:]] + [str(i) for i in f1S.tree.item(node)['values'][2:]] + [str(i) for i in f2S.tree.item(node)['values'][2:]]
+            f.write('\n' + '\t'.join(w))
+        f.close()
+        sys.stdout.write('%s successfully created\n' % (save))
+    except: sys.stdout.write('\n')
+
+    
+def genXML():
+
+    try:
+        maxnodes = tkSimpleDialog.askinteger(parent = root, title = 'Number of nodes', prompt = 'Input an integer greater than one')
+        if maxnodes < 2: return
+        filename = tkFileDialog.asksaveasfilename(parent = root, initialfile = "autogen.xml")
+        from random import randint
+        closes = 1; nodes = 1
+        f = open(filename, 'w')
+        last = '<Node Id="NoLabel" HG="%s">\n' % ','.join(['x' for i in range(randint(0,12))])
+        f.write(last)
+        subtree = True
+        while nodes < (maxnodes - 1):
+            nl = randint(-1000,1000) if closes > (sqrt(maxnodes)*0.75) and last[-3] == '/' \
+                 else (randint(-2,1) if last[-3] == '"' \
+                       else (randint(-2,6) if closes > 1 \
+                             else 1))
+            if nl <= 0:
+                last = '\t' * closes + '<Node Id="NoLabel" HG="%s" />\n' % ','.join(['x' for i in range(randint(0,6))])
+                nodes += 1
+            elif nl in [1,2,3]:
+                last = '\t' * closes + '<Node Id="NoLabel" HG="%s">\n' % ','.join(['x' for i in range(randint(0,6))])
+                closes += 1; nodes += 1
+            else:
+                closes -= 1
+                last = '\t' * closes + '</Node>\n'
+            f.write(last)
+        if last[-3] == '"':
+            f.write('\t' * closes + '<Node Id="NoLabel" HG="%s" />\n' % ','.join(['x' for i in range(randint(0,6))]))
+        while closes > 0:
+            f.write('\t' * (closes-1) + '</Node>\n')
+            closes -= 1
+        f.close()
+    except: sys.stdout.write('\n')
+
+        
+def findNode():
+
+    current = root.nametowidget(root.focus_get())
+    try:
+        nid = tkSimpleDialog.askstring(parent = root, title = 'Select node', prompt = 'Input a valid Node ID')
+        current.see(nid)
+        current.selection_set(nid)
+    except: pass
+
+
+optmenu.add_command(label = "Export tree (Newick format)", command = exportNewick)
+optmenu.add_command(label = "Show/Hide leaves", command = toggleLeaves)
+optmenu.add_command(label = "Find node", command = findNode)
+
+filemenu.add_command(label = "Open XML file", command = openXML)
+filemenu.add_command(label = "Import node types", command = openTypes)
+filemenu.add_command(label = "Generate random tree", command = genXML)
+filemenu.entryconfig(1, state = "disabled")
+filemenu.add_separator()
+filemenu.add_command(label = "Save current table (TSV)", command = saveTable) 
+filemenu.add_command(label = "Save all (TSV)", command = saveAll) 
+filemenu.entryconfig(4, state = "disabled")
+filemenu.entryconfig(5, state = "disabled")
+filemenu.add_separator()
+filemenu.add_command(label = "Quit", command = root.destroy)
+
+menubar.add_cascade(label = "File", menu = filemenu)
+menubar.add_cascade(label = "Options", menu = optmenu)
+menubar.entryconfig(2, state = "disabled")
+
+root.config(menu = menubar)
+root.mainloop()
--- tree.py
+++ tree.py
@@ -1,22 +1,51 @@
 # -*- coding: utf-8 -*-
 
-import re
+'''''''''''''''''''''''''''''''''
+!!! review f2/f2plus criteria !!!
+'''''''''''''''''''''''''''''''''
+
+import re, sys
 from collections import defaultdict as ddict, OrderedDict as odict
 from copy import copy
-from node import Node
+from math import sqrt, exp
 
+
+class Node:
+
+    def __init__(self, name, parent, mutations, layer):
+
+        self.name = name
+        self.parent = parent
+        self.children = []
+        self.siblings = []
+        self.mutations = mutations.split(',') if mutations else []
+        self.layer = layer
+        self.type = [0,0,0,0]
+        self.extra = {i:'--' for i in ('Rho','Age','SE','CI')}
+
+
+    def isSource(self):
+
+        t = self.type
+        if t == [0,0,1,0] and self.children == []: return "Sink"
+        elif (t == [1,0,0,0] or t == [0,1,0,0]) and self.children == []: return "Source"
+        elif (t == [0,0,0,1] or t == [0,0,0,0]) and self.children == []: return "Undefined"
+        else: return t
+
+        
 class Tree:
 
-    def __init__(self, source, types = ''):
+    def __init__(self, source):
 
         self.tree = odict()
         self.nodes = []
         self.leaves = []
         self.layers = odict()
         self.noLabel = 0
+        self.subtrees = {}         
         self.buildTree(source)
-        self.updateTypes(types)
         self.root = self.tree.values()[0]
+
 
     def buildTree(self, source):
 
@@ -24,94 +53,209 @@ class Tree:
             f = open(source,'r')
             layers, l = ddict(list), 1
             layers[0] = None
-            for i in f.readlines():
-                match = re.search('Id=(.*) HG=(.*)>', i)
-                if "/Node" in i: l -= 1
+            F = f.readlines(); f.close()
+            Fl = len(F)
+            for i in range(Fl):
+                sys.stdout.write('\rLoading file... %s/%s' % (i+1, Fl))
+                match = re.search('Id=(.*) HG=(.*)>', F[i])
+                if "/Node" in F[i]: l -= 1
                 elif match is None: pass
                 else:
-                    node = Node(match.group(1).strip('" '), layers[l-1],
-                                match.group(2).strip('" /,'), l)
+                    node = Node(match.group(1).strip('" '), layers[l-1], match.group(2).strip('" /,'), l)
                     if node.name == "NoLabel":
                         self.noLabel += 1
                         node.name = node.name + "_%s" % self.noLabel
                     self.tree[node.name] = node
+                    if l >= 2: self.tree[layers[l-1]].children.append(node.name)
                     if not node.layer in self.layers:
                         self.layers[node.layer] = [node.name]
                     else: self.layers[node.layer].append(node.name)
-                    if not "/>" in i:
+                    if not "/>" in F[i]:
                         layers[l] = node.name; l += 1
-            f.close()
-        elif type(source) == type(self.tree):
-            self.tree = source
-        else: print "Invalid input"; return ''
-
-        for node in self.tree.values():
-            node.children = [i.name for i in self.tree.values()
-                             if i.parent == node.name]
-            if len(node.children) > 0: self.nodes.append(node.name)
-            else:
-                self.leaves.append(node.name)
-
-            node.siblings = [i.name for i in self.tree.values()
-                             if i.parent == node.parent]
-            node.siblings.remove(node.name)
+                        self.nodes.append(node.name)
+                        self.subtrees[node.name] = odict(([node.name, node],))
+                    else: self.leaves.append(node.name)
+                    for x in layers.values()[1:l+1]:
+                        if node.layer > self.tree[x].layer: self.subtrees[x][node.name] = copy(node)
+            sys.stdout.write('\n') 
+            
+        else: sys.stdout.write('\n')
 
 
-    def removeNode(self, node):
+    def updateSubs(self, j, c):
 
-        for child in self.tree[node].children:
-            if child in self.tree: self.removeNode(child)
-        x = self.tree.pop(node)
-        if node in self.leaves: self.leaves.remove(node)
-        else: self.nodes.remove(node)
-        
-
-    def subtree(self, root):
-        print 1,
-        subtree = odict()
-        idx = self.tree.keys().index(root)
-        layer = self.tree[root].layer
-        subtree[root] = copy(self.tree[root])
-        while idx < len(self.tree)-1:
-            idx += 1
-            node = self.tree[self.tree.keys()[idx]]
-            if node.layer > layer:
-                subtree[node.name] = copy(node)
-            else: break
-        return subtree
+        for sub in self.subtrees.values(): 
+            for k in sub.keys(): sub[k] = copy(self.tree[k])
+            j += 1
+            sys.stdout.write('\rUpdating types... %s/%s' % (j,(c + len(self.tree) + len(self.subtrees))))
 
 
     def updateTypes(self, types = ''):
 
-        if types == '': pass
+        if types == '': return
         else:
+            for i in self.tree.values(): i.type = [0,0,0,0]
             f = open(types, 'r')
             codes = f.readlines()
             f.close()
+            j = 0
             for line in codes:
                 line = line.strip('\n').split('\t')
                 if line[0] in self.leaves:
                     leaf = self.tree[line[0]]
                     if line[1] in ["Sink", "sink", "SINK"]:
-                        leaf.type = [0,0,1]
+                        leaf.type = [0,0,1,0]
                     elif line[1] in ["Source","source", "SOURCE"]:
-                        leaf.type = [1,0,0] if \
-                        leaf.mutations != [] else [0,1,0]
-                    else: leaf.type = [0,0,0]
-        self.updateNodes()
+                        leaf.type = [1,0,0,0] if leaf.mutations != [] else [0,1,0,0]
+                    else: leaf.type = [0,0,0,1]
+                j += 1
+                sys.stdout.write('\rUpdating types... %s/%s' % (j,(len(codes) + len(self.tree) + len(self.subtrees))))
+        self.updateNodes(j, len(codes))
+        self.updateSubs(j + len(self.tree), len(codes))
+        self.nsrc, self.nsnk, self.nudf = 0, 0, 0
+        for i in self.leaves:
+            if self.tree[i].isSource() == "Source": self.nsrc += 1
+            elif self.tree[i].isSource() == "Sink": self.nsnk += 1
+            else: self.nudf += 1
+        sys.stdout.write('\n')
 
-    def updateNodes(self):
+        
+    def updateNodes(self, j, c):
 
         for layer in range(len(self.layers), 1, -1):
             for node in self.layers[layer]:
                 parent = self.tree[node].parent
-                if node in self.leaves and \
-                   self.tree[node].type != [0,0,0]:                    
-                    for i in range(3):
-                        self.tree[parent].type[i] += \
-                                self.tree[node].type[i]
+                if node in self.leaves and self.tree[node].isSource() != "Undefined":                    
+                    for i in range(4):
+                        self.tree[parent].type[i] += self.tree[node].type[i]
+                elif node in self.leaves and self.tree[node].isSource() == "Undefined":
+                    self.tree[parent].type[3] += 1
                 elif node in self.nodes:
-                    if self.tree[node].type[:1] > [0,0]:
+                    if self.tree[node].type[:2] > [0,0]:
                         self.tree[parent].type[0] += 1
-                    else: self.tree[parent].type[2] += 1
+                    if self.tree[node].type[2] >= 1:    #elif
+                        self.tree[parent].type[2] += 1
+                    else: self.tree[parent].type[3] += 1
+                j += 1
+                sys.stdout.write('\rUpdating types... %s/%s' % (j,(c + len(self.tree) + len(self.subtrees))))
+                
+
+    def Newick(self, node, string = ''):
+
+        if node == None:
+            children = self.layers[1]
+            string = ')'
+            for child in children: 
+                string = ',' + self.Newick(child, string)
+            string = '(' + string[1:]
+        else:
+            string = '%s' % (node) + string
+            if self.tree[node].children == []: return string
+            string = ')' + string
+            for child in self.tree[node].children:
+                string = ',' + self.Newick(child, string)
+            string = '(' + string[1:]
+        return string
+                
+
+    def Rho(self, root, sub, f = False):
+
+        if not f and self.tree[root].extra['Rho'] != '--':
+            return self.tree[root].extra['Rho']
+        else:
+            mutCount = 0
+            for leaf in (set(sub.keys()) & set(self.leaves)):
+                mutCount += self.mutationCount(root, leaf)
+            rho = float(mutCount)/ len((set(sub.keys()) & set(self.leaves)))
+            self.tree[root].extra['Rho'] = rho
+            return round(rho, 3)
             
+
+    def mutationCount(self, root, node, mutCount = 0):
+
+        mutCount += len(self.tree[node].mutations)
+        parent = self.tree[node].parent
+        if parent != root:
+            mutCount = self.mutationCount(root, parent, mutCount)
+        return mutCount
+
+
+    def removeNode(self, sub, node):
+
+        for child in self.tree[node].children:
+            if child in sub.keys(): sub = self.removeNode(sub, child)
+        x = sub.pop(node)
+        return sub
+
+    def Age(self, node):
+
+        rho = self.Rho(node, self.subtrees[node])
+        age = (exp(-exp(-0.0263 *(rho + 40.2789))) *rho *3624)
+        return round(age, 3)
+
+
+    def StErr(self, sub, f = False):
+
+        if not f and sub.values()[0].extra['SE'] != '--':
+            return sub.values()[0].extra['SE']
+        tSum = 0.0
+        leaves = set(sub.keys()) & set(self.leaves)
+        nodes = set(sub.keys()[1:]) & set(self.nodes)
+        for leaf in leaves: tSum += len(self.tree[leaf].mutations)
+        for nd in nodes:
+            if not f: lv = set(self.subtrees[nd].keys()) & set(self.leaves)
+            else: lv = set(self.subtrees[nd].keys()) & set([i for i in self.leaves if self.tree[i].isSource() == 'Sink'])
+            tSum += len(self.tree[nd].mutations) * len(lv) **2
+        se = sqrt(tSum/(len(leaves)**2))
+        if not f: sub.values()[0].extra['SE'] = se
+        return round(se, 3)
+
+
+    def ConfidenceInterval(self, node):
+
+        rho = self.Rho(node, self.subtrees[node])
+        se = self.StErr(self.subtrees[node])
+        lower = max(exp(-exp(-0.0263 * ((rho - (1.96 * se)) + 40.2789))) * (rho - (1.96 * se)) * 3624.0, 0)
+        upper = exp(-exp(-0.0263 * ((rho + (1.96 * se)) + 40.2789))) * (rho + (1.96 * se)) * 3624.0;
+        return round(lower, 3), round(upper, 3)
+
+
+    def fN(self, node, N = 1):
+
+        t = self.tree[node].type
+        if N == 1:
+            if t[0] >= N and t[2] > 0:
+                self.tree[node].extra['f1'] = True
+                return self.fStats(node, N)
+            else: return ['N/A','N/A','N/A']
+        elif N == 2:
+            if t[0] >= N:
+                self.tree[node].extra['f2'] = True                
+                return self.fStats(node, N)
+            else: return ['N/A','N/A','N/A']
+
+        
+    def fStats(self, node, N):
+
+        sub = copy(self.subtrees[node])
+        for i in range(len(sub)-1, 0, -1):
+            tp = sub.values()[i].type
+            if tp[0] > (N-1) or sub.values()[i].isSource() in ["Source", "Undefined"]: #or tp[1] > (N-1)
+                sub = self.removeNode(sub, sub.keys()[i])
+        leaves = set(sub.keys()) & set(self.leaves)
+        if len(leaves) == 0: return [0,'--','--']
+        elif len(leaves) == 1: return [1,0,0]
+        else:
+            return len(leaves), self.Rho(node, sub, True), self.StErr(sub, True)
+
+
+    def f2plus(self, node):
+
+        parent  = self.tree[node].parent
+        if parent == None: self.tree[node].extra['f2+'] = True
+        elif 'f2' in self.tree[node].extra.keys() and 'f2' in self.tree[parent].extra.keys():
+            self.tree[node].extra['f2+'] = True
+        else: self.tree[node].extra['f2+'] = False
+        return self.tree[node].extra['f2+']
+
+
